8e551eca61ce81c575cb80f4334d1f635e5f8aba
Kia Fathi <kiafathi@me.com>
2014-06-24 16:42:13 -0700

    further changes to implement serving fixed html

--- server.js
+++ server.js
@@ -18,21 +18,21 @@ fs.readFile('./client/index.html', function(err, html){
   if (err){
     throw err;
   }
-  http.createServer(function(request, response){
+  var server = http.createServer(function(request, response){
     response.writeHeader(200, {"Content-Type": "text/html"});
     response.write(html);
     response.end();
 
-  }).listen(8000);
+  }).listen(port);
 });
 
 /* We use node's http module to create a server. Note, we called it 'server', but
 we could have called it anything (myServer, blahblah, etc.). The function we pass it (handleRequest)
 will, unsurprisingly, handle all incoming requests. (ps: 'handleRequest' is in the 'request-handler' file).
 Lastly, we tell the server we made to listen on the given port and IP. */
-var server = http.createServer(hReq.handler);
-console.log("Listening on http://" + ip + ":" + port);
-server.listen(port);
+// var server = http.createServer(hReq.handler);
+// console.log("Listening on http://" + ip + ":" + port);
+// server.listen(port);
 
 // module.exports.server = server;
 /* To start this server, run:
